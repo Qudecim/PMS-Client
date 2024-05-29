1e1273fa86c1d74d16ab4c0347565408a67549cb
Zaichenko Yevgeniy <99prblmsaint1@gmail.com>
2024-05-30 01:11:27 +0300

    _

--- src/Client.php
+++ src/Client.php
@@ -9,6 +9,8 @@ class Client {
     private WebClient $connection;
     private IdGenerator $idGenerator;
 
+    private array $data;
+
     private function __construct(WebClient $connection, IdGenerator $idGenerator)
     {
         $this->connection = $connection;
@@ -23,29 +25,19 @@ class Client {
 
     public function get(string $key): Response {
         $request = new Request($this->idGenerator->generate(), 'g', $key, '');
+        $this->send($request);
 
-        try {
-            $this->connection->send($request->asString());
-            $responseString = $this->connection->receive();
-        } catch (\Exception $_) {
-            return Response::createError($request->getId(), Errors::ERROR_SEND_FAILED);
+        if (isset($this->data[$request->getId()])) {
+            return $this->data[$request->getId()];
         }
 
-        if (!is_string($responseString)) {
-            return Response::createError($request->getId(), Errors::ERROR_BODY_INCORRECT);
+        if ($this->wait($request->getId())) {
+            $response = $this->data[$request->getId()];
+            unset($this->data[$request->getId()]);
+            return $response;
         }
 
-        $rBody = json_decode($responseString, true);
-        if ($rBody === null) {
-            return Response::createError($request->getId(), Errors::ERROR_BODY_INCORRECT_DECODE);
-        }
-
-        $response = Response::createFromArray($rBody);
-        if ($request->getId() !== $response->getId()) {
-            return Response::createError($request->getId(), Errors::ERROR_DIFFERENT_ANSWER);
-        }
-
-        return $response;
+        throw new \Exception('1');
     }
 
     public function set(string $key, string $value): void {
@@ -54,4 +46,82 @@ class Client {
             $this->connection->send($request->asString());
         } catch (\Exception $e) {}
     }
+
+    public function push(string $key, string $itemKey): Response {
+        $request = new Request($this->idGenerator->generate(), 'p', $key, $itemKey);
+        $this->send($request);
+
+        if (isset($this->data[$request->getId()])) {
+            return $this->data[$request->getId()];
+        }
+
+        if ($this->wait($request->getId())) {
+            $response = $this->data[$request->getId()];
+            unset($this->data[$request->getId()]);
+            return $response;
+        }
+
+        throw new \Exception('1');
+    }
+
+    public function pull(string $key): Response {
+        $request = new Request($this->idGenerator->generate(), 'u', $key, '');
+        $this->send($request);
+
+         if (isset($this->data[$request->getId()])) {
+             return $this->data[$request->getId()];
+         }
+
+         if ($this->wait($request->getId())) {
+             $response = $this->data[$request->getId()];
+             unset($this->data[$request->getId()]);
+             return $response;
+         }
+
+         throw new \Exception('1');
+    }
+
+    private function wait(string $id): bool
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $responseString = $this->connection->receive();
+            $response = $this->getResponse($responseString);
+            $this->data[$response->getId()] = $response;
+            if ($id == $response->getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function send(Request $request): void
+    {
+        $this->connection->send($request->asString());
+    }
+
+    private function getResponse(string $responseString): Response
+    {
+        $rBody = json_decode($responseString, true);
+        $response = Response::createFromArray($rBody);
+        return $response;
+    }
+
+    //    private function getResponse($request, $responseString): Response
+    //    {
+    //        if (!is_string($responseString)) {
+    //            return Response::createError($request->getId(), Errors::ERROR_BODY_INCORRECT);
+    //        }
+    //
+    //        $rBody = json_decode($responseString, true);
+    //        if ($rBody === null) {
+    //            return Response::createError($request->getId(), Errors::ERROR_BODY_INCORRECT_DECODE);
+    //        }
+    //
+    //        $response = Response::createFromArray($rBody);
+    //        if ($request->getId() !== $response->getId()) {
+    //            return Response::createError($request->getId(), Errors::ERROR_DIFFERENT_ANSWER);
+    //        }
+    //
+    //        return $response;
+    //    }
 }
--- src/Response.php
+++ src/Response.php
@@ -5,23 +5,25 @@ namespace PMS;
 class Response {
     private string $id;
     private string $value;
+    private array $items;
     private int $error;
 
-    private function __construct(string $id, string $value, int $error)
+    private function __construct(string $id, string $value, array $items, int $error)
     {
         $this->id = $id;
         $this->value = $value;
+        $this->items = $items;
         $this->error = $error;
     }
 
     public static function createFromArray(array $item): self
     {
-        return new self($item['i'], $item['v'], $item['e']);
+        return new self($item['i'], $item['v'], $item['t'] ?? [], $item['e']);
     }
 
     public static function createError(string $id, int $error): self
     {
-        return new self($id, '', $error);
+        return new self($id, '', [], $error);
     }
 
     public function getId(): string
@@ -32,6 +34,11 @@ class Response {
     public function getValue(): string
     {
         return $this->value;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
     }
 
     public function getError(): int
