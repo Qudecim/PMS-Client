6cde9dad4578199204f2fdfcfd8154ea9a74391b
Zaichenko Yevgeniy <99prblmsaint1@gmail.com>
2024-05-30 00:48:01 +0300

    _

--- example/main.php
+++ example/main.php
@@ -6,15 +6,25 @@ $pms = \PMS\Client::create('ws://localhost:8080/ws');
 
 $time_start = microtime(true);
 
-for ($i = 0; $i < 100000; $i++) {
-    $pms->set('test_key' . $i, 'test_value' . $i);
-    $response = $pms->get('test_key' . $i);
-    if (!$response->isError()) {
-        //echo $response->getValue() . PHP_EOL;
-    } else {
-        echo 'error:' . $response->getError() . PHP_EOL;
-    }
-}
+$pms->set('test_key1', 'test_value1');
+$pms->set('test_key2', 'test_value2');
+$response = $pms->get('test_key1');
+echo $response->getValue() . PHP_EOL;
+
+
+$pms->push('list_key1', 'test_key1');
+$pms->push('list_key1', 'test_key2');
+$response = $pms->pull('list_key1');
+echo implode(',', $response->getItems()) . PHP_EOL;
+
+
+$pms->set('test_key1', 'test_value3');
+$response = $pms->get('test_key1');
+echo $response->getValue() . PHP_EOL;
+
+
+$response = $pms->pull('list_key1');
+echo implode(',', $response->getItems()) . PHP_EOL;
 
 $time_end = microtime(true);
 $execution_time = $time_end - $time_start;
